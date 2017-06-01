03c4e0051164a9c32443c5ffb961d463e51ffa06
Benedikt Huber <benedikt@vuframelab.com>
2017-06-01 17:47:19 +0200

    first commit

---
+++ sudoku.js
@@ -0,0 +1,133 @@
+$(document).ready(function() {
+    var sudoku = function() {
+
+        var fieldInfo = {};
+        var test = new Test();
+        
+        test.fillSudoku();
+        prepareFields($('#field_00'));
+        $('#solve-button').click(init);
+
+        function init() {
+            solve($('#field_00'));
+        }
+
+        function solve(currField) {
+            var currID = currField.attr('id').slice(-2);
+            if(fieldInfo[currID] === 'initial') {
+                solve(nextField(currField));
+                return;
+            }
+            for(var i = 1; i < 10; i++) {
+                if (canWriteToField(i, currField)) {
+                    console.log("writing to Field: " + currField.attr('id') + ", value: " + i);
+                    currField.val(i);
+                    var next = nextField(currField);
+                    solve(next);
+                }
+            }
+            currField.val('');
+        }
+
+        function nextField(currField) {
+            var currID = currField.attr('id').slice(-2);
+            var next;
+            if(currID[1] < 8) {
+                next = $('#field_' + currID[0] + (parseInt(currID[1]) + 1).toString());
+            } else {
+                next = $('#field_' + (parseInt(currID[0]) + 1).toString() + '0');
+            }
+            if(!next.length) return false;
+            else return next;
+        }
+
+        function canWriteToField(value, field) {
+            return !verticalCollision(value, field) &&
+                   !horizontalCollision(value, field) &&
+                   !squareCollision(value, field);
+        }
+
+        function verticalCollision(value, field) {
+            var id = field.attr('id').slice(-2);
+            for(var i = 1; i < 10; i++) {
+                var currField = $('#field_' + i + id[1]);
+                if(currField.val() == value) {
+                    console.log("collision with number: " + value + " at " + currField.attr('id'));
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function horizontalCollision(value, field) {
+            var id = field.attr('id').slice(-2);
+            for(var i = 1; i < 10; i++) {
+                var currField = $('#field_' + id[0] + i);
+                if(currField.val() == value) {
+                    console.log("collision with number: " + value + " at " + currField.attr('id'));
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function squareCollision(value, field) {
+            var id = field.attr('id').slice(-2);
+            var idRangeX = getIdRange(id[0]);
+            var idRangeY = getIdRange(id[1]);
+
+            for(var x of idRangeX)
+                for(var y of idRangeY) {
+                    currID = x.toString() + y.toString();
+                    if(currID === id)
+                        continue;
+                    var currField = $('#field_' + currID);
+                    if(currField.val() == value) {
+                        console.log("collision with number: " + value + " at " + currID)
+                        return true;
+                    }
+                }
+            return false;
+        }
+
+        function getIdRange(id) {
+            var id = parseInt(id);
+            switch(id) {
+                case 0:
+                case 1:
+                case 2:
+                    return [0,1,2];
+                case 3:
+                case 4:
+                case 5:
+                    return [3,4,5];
+                case 6:
+                case 7:
+                case 8:
+                    return [6,7,8];
+            }
+        }
+
+        function prepareFields(field) {
+            console.log(field.attr('id'));
+            var id = field.attr('id').slice(-2);
+
+            if(field.val() !== '')
+                fieldInfo[id] = 'initial';
+            else
+                fieldInfo[id] = 'unused';
+
+            var next = nextField(field);
+            if(!next)
+                return false;
+            if (!prepareFields(next))
+                return false;
+        }
+        // sleep time expects milliseconds
+        function sleep (time) {
+          return new Promise((resolve) => setTimeout(resolve, time));
+        }
+
+        // Usage!
+    }();
+})
---
+++ test.js
@@ -0,0 +1,48 @@
+var Test = function() {
+    this.fillSudoku = function(){
+        document.getElementById('field_03').value = 2;
+        document.getElementById('field_04').value = 6;
+        document.getElementById('field_06').value = 7;
+        document.getElementById('field_08').value = 1;
+
+        document.getElementById('field_10').value = 6;
+        document.getElementById('field_11').value = 8;
+        document.getElementById('field_14').value = 7;
+        document.getElementById('field_17').value = 9;
+
+        document.getElementById('field_20').value = 1;
+        document.getElementById('field_21').value = 9;
+        document.getElementById('field_25').value = 4;
+        document.getElementById('field_26').value = 5;
+
+        document.getElementById('field_30').value = 8;
+        document.getElementById('field_31').value = 2;
+        document.getElementById('field_33').value = 1;
+        document.getElementById('field_37').value = 4;
+
+        document.getElementById('field_42').value = 4;
+        document.getElementById('field_43').value = 6;
+        document.getElementById('field_45').value = 2;
+        document.getElementById('field_46').value = 9;
+
+        document.getElementById('field_51').value = 5;
+        document.getElementById('field_55').value = 3;
+        document.getElementById('field_57').value = 2;
+        document.getElementById('field_58').value = 8;
+
+        document.getElementById('field_62').value = 9;
+        document.getElementById('field_63').value = 3;
+        document.getElementById('field_67').value = 7;
+        document.getElementById('field_68').value = 4;
+
+        document.getElementById('field_71').value = 4;
+        document.getElementById('field_74').value = 5;
+        document.getElementById('field_77').value = 3;
+        document.getElementById('field_78').value = 6;
+
+        document.getElementById('field_80').value = 7;
+        document.getElementById('field_82').value = 3;
+        document.getElementById('field_84').value = 1;
+        document.getElementById('field_85').value = 8;
+    }
+};
